68128472b70a3c4a7eaaed3dd8970076bf5a819f
Josh Mock <josh@joshmock.com>
2023-06-21 15:38:18 -0500

    Sway tweaks

--- home/dot_config/sway/scripts/executable_weather.sh
+++ home/dot_config/sway/scripts/executable_weather.sh
@@ -3,13 +3,10 @@
 LOC="$1"
 # HTML encode string as %20
 LOCATION=$(sed -e "s/ /%20/g" <<<"$LOC")
-content=$(curl -sS "https://thisdavej.azurewebsites.net/api/weather/current?loc=NewYork&deg=C")
+content=$(curl -sS https://wttr.in/?format=j1)
+content=$(curl -sS "https://thisdavej.azurewebsites.net/api/weather/current?loc=$LOCATION&deg=F")
 ICON=$(curl -s 'https://wttr.in/?format=1' | sed 's/[+0-9a-cA-Z°-]//g' )
-# echo $ICON
-TEMP=$(echo $content | jq -r '. | "\(.temperature)°\(.degType)"' | sed 's/"//g')
-TOOLTIP=$(echo $content | jq -r '. | "\(.temperature)°\(.degType)\n\(.skytext)"' | sed 's/"//g')
-CLASS=$(echo $content | jq .skytext)
+current="$(echo "$content" | jq -r .current_condition)"
+TEMP="$(echo "$current" | jq -r .temp_F)"
+TOOLTIP=$(echo $current | jq -r '. | "\(.temp_F)°F\nFeels like \(.FeelsLikeF)"' | sed 's/"//g')
 echo '{"text": "'$TEMP'", "tooltip": "'$ICON $TOOLTIP $LOC'", "class": '$CLASS' }'
-
-
-
