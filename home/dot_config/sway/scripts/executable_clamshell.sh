188585e01bce0cfbf6c56d515a8eeebbb3f54943
Josh Mock <josh@joshmock.com>
2023-06-23 10:36:43 -0500

    Sway tweaks

--- home/dot_config/sway/scripts/executable_clamshell.sh
+++ home/dot_config/sway/scripts/executable_clamshell.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 if cat /proc/acpi/button/lid/*/state | grep -q open; then
     swaymsg output eDP-1 enable
+    swaymsg output DP-2 enable
 else
     swaymsg output eDP-1 disable
+    swaymsg output DP-2 disable
 fi
