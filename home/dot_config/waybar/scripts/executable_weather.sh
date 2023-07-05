dc7c27c4d90a4227cc8b954d18ceeb31aa075a03
Josh Mock <josh@joshmock.com>
2023-07-05 09:20:35 -0500

    More sway/waybar tweaks

--- home/dot_config/waybar/scripts/executable_weather.sh
+++ home/dot_config/waybar/scripts/executable_weather.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 content=$(curl -sS https://wttr.in/Nashville\?format=j1)
-ICON=$(curl -s 'https://wttr.in/Nashville\?format=1' | sed 's/[+0-9a-cA-Z°-]//g' )
 current="$(echo "$content" | jq -r '.current_condition[0]')"
 TEMP="$(echo "$current" | jq -r .temp_F)"
-TOOLTIP=$(echo "$current" | jq -r '. | "\(.temp_F)°F\nFeels like \(.FeelsLikeF)°F"' | sed 's/"//g')
-echo "{\"text\": \"$TEMP\", \"tooltip\": \"$ICON $TOOLTIP\", \"class\": \"\" }"
+TOOLTIP=$(echo "$current" | jq -r '. | "\(.temp_F)°F, feels like \(.FeelsLikeF)°F"' | sed 's/"//g')
+echo "{\"text\": \"$TEMP°F\", \"tooltip\": \"$TOOLTIP\", \"class\": \"\" }"
