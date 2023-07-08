5936986023dad5c51e55c55652df42c0f13c8203
Josh Mock <josh@joshmock.com>
2023-07-08 12:57:36 -0500

    Updates to personal laptop

--- home/dot_config/sway/scripts/executable_clamshell.sh
+++ home/dot_config/sway/scripts/executable_clamshell.sh
@@ -2,9 +2,11 @@
 if cat /proc/acpi/button/lid/*/state | grep -q open; then
     swaymsg output eDP-1 enable
     swaymsg output DP-2 enable
+    swaymsg output LVDS-1 enable
     swaymsg output DP-3 disable
 else
     swaymsg output eDP-1 disable
     swaymsg output DP-2 disable
     swaymsg output DP-3 disable
+    swaymsg output LVDS-1 disable
 fi
--- home/dot_config/sway/scripts/executable_weather.sh
+++ home/dot_config/sway/scripts/executable_weather.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-content=$(curl -sS https://wttr.in/Nashville\?format=j1)
+content=$(curl -sS 'https://wttr.in/?format=j1')
 current="$(echo "$content" | jq -r '.current_condition[0]')"
 TEMP="$(echo "$current" | jq -r .temp_F)"
 TOOLTIP=$(echo "$current" | jq -r '. | "\(.temp_F)°F, feels like \(.FeelsLikeF)°F"' | sed 's/"//g')
