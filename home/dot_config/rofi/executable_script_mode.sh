5f521ec6bf5c5a0effb36557a61cbe23fbb30c9f
Josh Mock <josh@joshmock.com>
2023-11-01 13:17:58 -0500

    Go back to Rofi for now

--- home/dot_config/rofi/executable_script_mode.sh
+++ home/dot_config/rofi/executable_script_mode.sh
@@ -5,7 +5,7 @@ MAP="$WORKINGDIR/cmd.csv"
 
 cat "$MAP" \
     | cut -d ',' -f 1 \
-    | wofi -dmenu -p "Util " -show combi -modi combi \
+    | rofi -dmenu -p "Util " -show combi -modi combi \
     | head -n 1 \
     | xargs -i --no-run-if-empty grep "{}" "$MAP" \
     | cut -d ',' -f 2 \
