830da4c07c6c800444d1e74c358bda51e4ade9d6
Josh Mock <josh@joshmock.com>
2024-04-18 09:49:09 -0500

    fix(neovim): switch back to vim-surround from mini.surround

--- home/dot_config/nvim/lua/plugins/surround.lua
+++ home/dot_config/nvim/lua/plugins/surround.lua
@@ -1,16 +1,7 @@
 return {
   {
     "echasnovski/mini.surround",
-    opts = {
-      mappings = {
-        add = "ys",
-        delete = "ds",
-        find = "",
-        find_left = "",
-        highlight = "",
-        replace = "cs",
-        update_n_lines = "",
-      },
-    },
+    enabled = false,
   },
+  { "tpope/vim-surround" },
 }
