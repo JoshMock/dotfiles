4d45f6efb3e53a294c62d2dce141d7e671121f9a
Josh Mock <josh@joshmock.com>
2024-04-22 15:40:59 -0500

    fix(neovim): support for ts-standard formatter on save

---
+++ home/dot_config/nvim/lua/plugins/typescript.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters = {
+        tsstandard = {
+          command = "ts-standard",
+          args = { "--fix", "--stdin", "--stdin-filename", "$FILENAME" },
+          stdin = true,
+          async = true,
+          cwd = require("conform.util").root_file({ "tsconfig.json" }),
+        },
+      },
+      formatters_by_ft = {
+        typescript = { "tsstandard" },
+      },
+    },
+  },
+}
