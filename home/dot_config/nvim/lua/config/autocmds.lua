d20358158bc274ab440d816ecdbf027c16102ff9
Josh Mock <josh@joshmock.com>
2024-07-29 10:37:07 -0500

    fix(neovim): don't store gitcommit sessions

--- home/dot_config/nvim/lua/config/autocmds.lua
+++ home/dot_config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,11 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
+local function augroup(name)
+  return vim.api.nvim_create_augroup("joshmock_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("disable_session_persistence"),
+  pattern = { "gitcommit" },
+  callback = function()
+    require("persistence").stop()
+  end,
+})
