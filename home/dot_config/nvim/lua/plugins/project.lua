62d8fc1582326f7161d5897ebb0afc417c0ca5aa
Josh Mock <josh@joshmock.com>
2024-08-20 09:23:27 -0500

    fix(neovim): more rules for defining projects

--- home/dot_config/nvim/lua/plugins/project.lua
+++ home/dot_config/nvim/lua/plugins/project.lua
@@ -3,7 +3,8 @@ return {
     "ahmedkhalf/project.nvim",
     opts = {
       manual_mode = false,
-      patterns = { ".git", "index.norg" },
+      patterns = { ".git", "index.norg", "package.json", ">Code" },
+      scope_chdir = "win",
     },
   },
 }
