5155aa3448347299c0e865a5ce5bc9e0b17e0f50
Josh Mock <josh@joshmock.com>
2024-11-19 09:50:01 -0600

    fix(nvim): ignore git worktrees in project list

--- home/dot_config/nvim/lua/plugins/project.lua
+++ home/dot_config/nvim/lua/plugins/project.lua
@@ -3,7 +3,7 @@ return {
     "ahmedkhalf/project.nvim",
     opts = {
       manual_mode = false,
-      patterns = { ".git", "index.norg", "package.json", ">Code" },
+      patterns = { ".git", "index.norg", "package.json", ">Code", "!.worktrees" },
       scope_chdir = "win",
     },
   },
