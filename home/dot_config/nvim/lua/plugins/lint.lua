8093d858503e8638cf077ae38b90721cdf2598b8
Josh Mock <josh@joshmock.com>
2024-11-26 16:09:01 -0600

    fix(nvim): customize markdown linting rules

---
+++ home/dot_config/nvim/lua/plugins/lint.lua
@@ -0,0 +1,17 @@
+local HOME = os.getenv("HOME")
+
+return {
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters = {
+        markdownlint = {
+          args = { "--disable", "MD013", "--" },
+        },
+        ["markdownlint-cli2"] = {
+          args = { "--config", HOME .. "/.markdownlint-cli2.yaml", "--" },
+        },
+      },
+    },
+  },
+}
