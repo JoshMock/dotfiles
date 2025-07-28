c00fff40a083ddace84626be878f4ce3f74c45b0
Josh Mock <josh@joshmock.com>
2025-07-28 11:59:19 -0500

    feat(nvim): reintroduce full neorg setup

--- home/dot_config/nvim-kickstart/init.lua
+++ home/dot_config/nvim-kickstart/init.lua
@@ -604,9 +604,42 @@ require("lazy").setup({
     "nvim-neorg/neorg",
     lazy = false,
     version = "*",
-    config = true,
+    config = function()
+      require("neorg").setup({
+        load = {
+          ["core.defaults"] = {},
+          ["core.concealer"] = {},
+          ["core.dirman"] = {
+            config = {
+              workspaces = {
+                work = "~/Documents/notes-work",
+                clients = "~/Documents/notes-work/clients",
+              },
+              default_workspace = "clients",
+            },
+          },
+          ["core.export"] = {},
+          ["external.interim-ls"] = {
+            config = {
+              completion_provider = {
+                enable = true,
+                documentation = true,
+                categories = false,
+              },
+            },
+          },
+        },
+      })
+    end,
   },
-  "benlubas/neorg-interim-ls",
+  {
+    "benlubas/neorg-interim-ls",
+    ft = { "norg", "neorg" },
+    lazy = true,
+    dependencies = {
+      "nvim-neorg/neorg",
+    },
+  },
 
   -- treesitter
   {
